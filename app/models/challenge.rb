b21e9ee41973c39f306765fe835ce552026cd3ad
Augusto Azevedo <augusto.azevedo.jr@gmail.com>
2017-10-03 17:07:55 -0700

    Write assossiations between models.

--- app/models/challenge.rb
+++ app/models/challenge.rb
@@ -1,2 +1,7 @@
 class Challenge < ApplicationRecord
+  belongs_to :proposer, class_name: :User
+  has_many :courses
+
+  validates :theme, {uniqueness: true, presence: true}
+  validates :course_submissions_deadline, :review_submissions_deadline, {presence: true}
 end
