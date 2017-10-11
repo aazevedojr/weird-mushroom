5048f225f21aa857a0cc7a6573c437c1d12fe108
Augusto Azevedo <augusto.azevedo.jr@gmail.com>
2017-10-10 17:26:15 -0700

    Fixed seedfile bugs.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,71 +1,118 @@
+# Helpers:
+
+def generate_bookmark
+  rand.to_s[2..11] + '-0000-' + rand.to_s[2..11] + '-' + rand.to_s[2..11]
+end
+
+def validate(instance)
+  if instance.save
+    puts "Created #{instance.class}: #{instance.id}"
+  else
+    instance.errors.messages.each do |error_message|
+      puts error_message
+    end
+  end
+end
+
 # Clean up:
+
+puts "Removing existing seed data..."
+
 User.delete_all
 Challenge.delete_all
 Course.delete_all
 Review.delete_all
 
+puts "Removed all existing seed data."
+
 # Me:
+
+puts "Creating users..."
+
 User.create(
   username: "gus",
   email: "gus@weirdmushroom.net",
   password: "gus"
 )
 
+puts "Created user: GUS"
+
 # Users:
+
 10.times do
-  User.create(
+  user = User.new(
     username: Faker::Internet.user_name,
     email: Faker::Internet.email,
     password: "password"
   )
+  validate(user)
 end
+
+users = User.all
 
 #Challenges:
-def generate_bookmark
-  rand.to_s[2..11] + '-0000-' + rand.to_s[2..11] + '-' + rand.to_s[2..11]
-end
 
-Challenge.create(
+puts "Creating challenges..."
+
+chal_1 = Challenge.new(
   theme: Faker::Job.key_skill,
   guidelines: Faker::TwinPeaks.quote,
   course_submissions_deadline: DateTime.now.next_day(7),
   review_submissions_deadline: DateTime.now.next_day(14),
-  proposer: User.all.sample
+  proposer: users.sample
 )
-Challenge.create(
+
+chal_2 = Challenge.new(
   theme: Faker::RockBand.name,
   guidelines: Faker::Simpsons.quote,
   course_submissions_deadline: DateTime.now.next_day(10),
   review_submissions_deadline: DateTime.now.next_day(15),
-  proposer: User.all.sample
+  proposer: users.sample
 )
 
+validate(chal_1)
+validate(chal_2)
+
 # Courses:
-User.all.each do |user|
-  Course.create(
+
+puts "Creating courses..."
+
+users.each do |user|
+  cour_1 = Course.new(
     bookmark: generate_bookmark,
     title: Faker::Book.title,
     maker: user,
     challenge: Challenge.first
   )
-  Course.create(
+
+  cour_2 = Course.new(
     bookmark: generate_bookmark,
     title: Faker::Pokemon.move,
     maker: user,
     challenge: Challenge.second
   )
+
+  validate(cour_1)
+  validate(cour_2)
+
 end
 
+courses = Course.all
+
 # Reviews:
-User.all.each do |user|
-  courses = Course.all
-  courses.delete(user.courses)
+
+puts "Creating reviews..."
+
+users.each do |user|
   courses.each do |course|
-    Review.create(
-      rating: rand(5),
-      feedback: Faker::HarryPotter.quote,
-      reviewer: user,
-      course: course
-    )
+    if course.maker != user
+      review = Review.new(
+        rating: rand(5),
+        feedback: Faker::HarryPotter.quote,
+        reviewer: user,
+        course: course
+      )
+      validate(review)
+    end
   end
 end
