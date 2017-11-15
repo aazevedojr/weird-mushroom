2b5c2384b52a1d4f6f8880001df67f4951f6b1c0
Augusto Azevedo <augusto.azevedo.jr@gmail.com>
2017-11-14 16:03:56 -0800

    Add title to static pages.

--- spec/features/visiting_homepage_feature_spec.rb
+++ spec/features/visiting_homepage_feature_spec.rb
@@ -3,6 +3,6 @@ require "rails_helper"
 RSpec.feature "Visiting the homepage" do
   scenario "User sees title for Home" do
     visit "/static_pages/home"
-    expect(page).to have_title ("Home | Weird Mushroom")
+    expect(page).to have_title ("WeirdMushroom | Home")
   end
 end
