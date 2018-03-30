d702a2e9e34925306a3ada02a66b94f3afe6d41a
Augusto Azevedo <augusto.azevedo.jr@gmail.com>
2018-03-30 12:11:05 -0700

    Re-write tests for static pages titles.

--- spec/features/visiting_homepage_feature_spec.rb
+++ spec/features/visiting_homepage_feature_spec.rb
@@ -2,7 +2,7 @@ require "rails_helper"
 
 RSpec.feature "Visiting the homepage" do
   scenario "User sees title for Home" do
-    visit "/static_pages/home"
+    visit root_path
     expect(page).to have_title ("WeirdMushroom | Home")
   end
 end
